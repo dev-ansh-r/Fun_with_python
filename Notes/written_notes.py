fb3981905a95e2c63091bfe4d56ff5edbb711b81
dev-ansh-r <rdevanshshukla26@gmail.com>
2022-11-12 22:53:50 +0530

    hand written notes generator

---
+++ Notes/written_notes.py
@@ -0,0 +1,4 @@
+import pywhatkit as kit
+kit.text_to_handwriting("Hello, My name is Devansh shukla and I am a Learner. I found this on the internet and wanted to try if it works", rgb=(0,0,0))
+
+#kit.text_to_handwriting("And it does", rgb=(0,0,0))
